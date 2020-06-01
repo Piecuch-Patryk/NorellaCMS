6bde88b2ffc95e9e875b6c90e9573f5d0a2339ee
Patryk Piecuch <patrykowo@gmail.com>
2020-06-01 12:06:32 +0200

    Add header laptop resolution.

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -2,29 +2,28 @@
     <a href="{{ route('home.index') }}">
         <img src="{{ asset('storage/assets/logo/norella.png') }}" class="logo" alt="Norella logo">
     </a>
-    {{-- <a href="tel:+48792888984" class="text-light text-underline d-lg-none mr-4">+48 792 888 984</a> --}}
     <button class="navbar-toggler border-0 p-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav text-center d-md-flex justify-content-center w-100 pt-xs-5 pt-5">
+        <ul class="navbar-nav text-center d-md-flex justify-content-center justify-content-lg-end w-100 pt-xs-5 pt-5 p-lg-0">
             <li class="nav-item {{ Route::is('home.index') ? 'active' : '' }}">
-                <a class="nav-link mx-md-3" href="{{ route('home.index') }}">Strona Główna</a>
+                <a class="nav-link mx-md-3 py-lg-1" href="{{ route('home.index') }}">Strona Główna</a>
             </li>
             <li class="nav-item {{ Route::is('product.index') ? 'active' : '' }}">
-                <a class="nav-link mx-md-3" href="{{ route('product.index') }}">Oferta</a>
+                <a class="nav-link mx-md-3 py-lg-1" href="{{ route('product.index') }}">Oferta</a>
             </li>
             <li class="nav-item {{ Route::is('product.showprice') ? 'active' : '' }}">
-                <a class="nav-link mx-md-3" href="{{ route('product.showprice') }}">Cennik</a>
+                <a class="nav-link mx-md-3 py-lg-1" href="{{ route('product.showprice') }}">Cennik</a>
             </li>
             <li class="nav-item {{ Route::is('review.index') ? 'active' : '' }}">
-                <a class="nav-link mx-md-3" href="{{ route('review.index') }}">Opinie</a>
+                <a class="nav-link mx-md-3 py-lg-1" href="{{ route('review.index') }}">Opinie</a>
             </li>
             <li class="nav-item {{ Route::is('gallery.index') ? 'active' : '' }}">
-                <a class="nav-link mx-md-3" href="{{ route('gallery.index') }}">Galeria</a>
+                <a class="nav-link mx-md-3 py-lg-1" href="{{ route('gallery.index') }}">Galeria</a>
             </li>
             <li class="nav-item {{ Route::is('about.index') ? 'active' : '' }}">
-                <a class="nav-link mx-md-3" href="{{ route('about.index') }}">O nas</a>
+                <a class="nav-link mx-md-3 py-lg-1" href="{{ route('about.index') }}">O nas</a>
             </li>
             {{-- Admin only --}}
             @auth
